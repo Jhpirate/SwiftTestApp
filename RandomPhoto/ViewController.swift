4f0b2d12395d783a76a672b1657709bb6af61d1a
Jared Heeringa <jaredheeringa@icloud.com>
2023-12-05 21:11:47 -0600

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Jared Heeringa on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
